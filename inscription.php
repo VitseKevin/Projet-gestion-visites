17e76b53d656a334ce87b4c34a9a0fd173cb427e
VitseKevin <147046649+VitseKevin@users.noreply.github.com>
2023-11-29 19:37:27 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -6,11 +6,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
 </head>
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
 
+        form {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+        }
+
+        input{
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background-color: #4caf50;
+            color: white;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
 <body>
     <h1>S'inscrire</h1>
 
-    <form action="../controller/passerelle_inscr.php" method="post">
+    <form action="../controlleur/passerelle_inscr.php" method="post">
         <label for="nom">Nom :</label>
         <input type="text" id="nom" name="nom">
 
