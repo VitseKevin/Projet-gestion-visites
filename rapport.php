de617ff3eb6887d86fb51a2d03707c10ac3386f4
luigicatala <152324003+luigicatala@users.noreply.github.com>
2023-11-29 19:09:33 +0100

    Update rapport.php

--- rapport.php
+++ rapport.php
@@ -5,22 +5,22 @@ class Rapport{
     private $idMedRap;
     private $dateRap;
     private $motifRap;
-    private $bilanRap;
-    private $idVisRap;  
-    
+    private $bilanRap; 
+    private $idVisRap;
 
-    public function __construct($idRap,$idMedRap,$dateRap,$motifRap,$bilanRap){
+    public function __construct($idRap,$idMedRap,$dateRap,$motifRap,$bilanRap, $idVisRap){
         $this->idRap = (int)$idRap;
         $this->idMedRap = (int)$idMedRap;
         $this->dateRap = (string)$dateRap;
         $this->motifRap = (string)$motifRap;
         $this->bilanRap = (string)$bilanRap;
+        $this->idVisRap = (int)$idVisRap;
     }
 
     public function addRapport($newRapport,$pdo){
-        $sql = "INSERT INTO rapport (idMedecin,date, motif, bilan) 
+        $sql = "INSERT INTO rapport (idMedecin,date, motif, bilan,idVisiteur) 
         VALUES ('$newRapport->idMedRap', '$newRapport->dateRap', '$newRapport->motifRap', 
-                '$newRapport->bilanRap')";
+                '$newRapport->bilanRap','$newRapport->idVisRap')";
 
         $pdo->query($sql);
     }
