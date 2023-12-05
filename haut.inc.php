2bd0c6a5192acea21561fbbc52c9510cfb5f9a11
sofiane7878 <152324055+sofiane7878@users.noreply.github.com>
2023-12-05 20:55:05 +0100

    Add files via upload

---
+++ haut.inc.php
@@ -0,0 +1,25 @@
+<!Doctype html>
+<html>
+<head>
+    <title>Mon Site</title>
+    <link rel="stylesheet" href="<?php echo RACINE_SITE; ?>inc/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+            
+</head>
+<body>
+<header>
+    <div class="conteneur">
+
+        <nav>
+            <a href="<?php echo RACINE_SITE; ?>../vue/acceuil.php">Acceuil</a>
+        </nav>
+    </div>
+</header>
+<section>
+    <div class="conteneur">
+
+
+
+
+
+
