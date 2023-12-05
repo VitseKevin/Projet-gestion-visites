5d420eb74c90bf9b29f4de4ee26a72d6b20a7b2c
sofiane7878 <152324055+sofiane7878@users.noreply.github.com>
2023-12-05 20:42:01 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -5,47 +5,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
-</head>
-<style>
+    <style>
         body {
             font-family: Arial, sans-serif;
             background-color: #f4f4f4;
             margin: 0;
             padding: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
+        }
+
+        h1 {
+            text-align: center;
+            color: #333;
         }
 
         form {
+            max-width: 600px;
+            margin: 20px auto;
             background-color: #fff;
             padding: 20px;
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
 
-        h1 {
-            text-align: center;
+        fieldset {
+            border: 1px solid #ddd;
+            border-radius: 5px;
+            margin-bottom: 15px;
+            padding: 10px;
+        }
+
+        legend {
+            font-size: 1.2em;
+            font-weight: bold;
+            color: #333;
         }
 
         label {
             display: block;
             margin-bottom: 8px;
+            color: #555;
         }
 
-        input{
+        input {
             width: 100%;
-            padding: 10px;
-            margin-bottom: 15px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
+            padding: 8px;
+            margin-bottom: 10px;
             box-sizing: border-box;
         }
 
         input[type="submit"] {
             background-color: #4caf50;
-            color: white;
+            color: #fff;
             cursor: pointer;
         }
 
@@ -53,33 +63,44 @@
             background-color: #45a049;
         }
     </style>
+</head>
+
 <body>
     <h1>S'inscrire</h1>
 
-    <form action="../controlleur/passerelle_inscr.php" method="post">
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom">
+    <form action="../controller/passerelle_inscr.php" method="post">
+        <fieldset>
+            <legend>Informations personnelles</legend>
 
-        <label for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="prenom">
+            <label for="nom">Nom :</label>
+            <input type="text" id="nom" name="nom">
 
-        <label for="login">Login :</label>
-        <input type="text" id="login" name="login">
+            <label for="prenom">Prénom :</label>
+            <input type="text" id="prenom" name="prenom">
 
-        <label for="mdp">Mot de Passe :</label>
-        <input type="text" id="mdp" name="mdp">
+            <label for="login">Login :</label>
+            <input type="text" id="login" name="login">
+        
 
-        <label for="adresse">Adresse :</label>
-        <input type="text" id="adresse" name="adresse">
+            <label for="mdp">Mot de Passe :</label>
+            <input type="password" id="mdp" name="mdp">
 
-        <label for="cp">Code Postal :</label>
-        <input type="number" id="cp" name="cp" min="0" max="100000">
+            <label for="adresse">Adresse :</label>
+            <input type="text" id="adresse" name="adresse">
 
-        <label for="ville">Ville :</label>
-        <input type="text" id="ville" name="ville">
+            <label for="cp">Code Postal :</label>
+            <input type="number" id="cp" name="cp" min="0" max="100000">
 
-        <label for="date_embauche">Date d'embauche :</label>
-        <input type="date" id="date_embauche" name="date_embauche" value="2023-11-17" min="2000-01-01" max="2023-12-31">
+            <label for="ville">Ville :</label>
+            <input type="text" id="ville" name="ville">
+        </fieldset>
+
+        <fieldset>
+            <legend>Date d'embauche</legend>
+
+            <label for="date_embauche">Date d'embauche :</label>
+            <input type="date" id="date_embauche" name="date_embauche" value="2023-11-17" min="2000-01-01" max="2023-12-31">
+        </fieldset>
 
         <input type="submit" value="Envoyer">
     </form>
